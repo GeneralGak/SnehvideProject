188209e68757b99bfeeb2e1a52d4a6c66bc48fd9
GeneralGak <57255134+GeneralGak@users.noreply.github.com>
2020-02-18 00:57:03 +0100

    Added EnemyWaves class and two methodes to the Mine class

--- SnehvideProject/SnehvideProject/Buildings/Mine.cs
+++ SnehvideProject/SnehvideProject/Buildings/Mine.cs
@@ -8,5 +8,17 @@ namespace SnehvideProject
 {
     class Mine : Building, IPlayerUnit
     {
+
+
+		public void EnterMine()
+		{
+			// TODO: Add a thread for every miner that enters the Mine.
+		}
+
+		public void ValueIncreaser()
+		{
+			// TODO: Add the functionality that adds value to the Treasury. (Or something else to keep track for gold)
+		}
+
     }
 }
